84850e7148bb9f6add403b75c193839ea1d3dc02
Prelang Builder <builder@prelang.com>
2014-07-08 18:24:33 +0000

    Generated scaffolding for model 'Company' via Rails

---
+++ app/views/companies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @company, :id, :email, :phone, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :companies
+
   resources :locations
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
